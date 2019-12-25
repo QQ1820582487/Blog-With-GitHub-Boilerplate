558b61e59f58ecf9ac2d46549a1aa0c32cb6d4cf
Xuhui01 <1820502487@qq.com>
2019-12-26 01:40:38 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,11 +17,11 @@ enable_jsdelivr = {
 site_name = "我的个人博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "Xuxx"
+email = "xuxx3319@gmail.com"
+author_homepage = "暂无"
+description = "只坚持一种正义——我的正义。"
+key_words = ['Maverick', 'Xuxx', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
@@ -32,7 +32,7 @@ external_links = [
     {
         "name": "三無計劃",
         "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
+        "brief": "Xuxx的主页。"
     }
 ]
 nav = [
